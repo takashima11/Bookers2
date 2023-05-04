bb175c8923c3d0991ce88c6dcf22b13b0e562715
takashima11 <129072192+takashima11@users.noreply.github.com>
2023-05-04 09:25:40 +0000

    [Add]4-27 viewの作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,25 +7,34 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 
   private
 
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title,:body,:image)
   end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+end
